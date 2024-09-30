a6c7b01085e2ad0d39466ce258a75b3823034a40
yoonion <yoonion97@gmail.com>
2024-09-30 18:48:44 +0900

    chore: project init

---
+++ src/main/java/com/task/penta/PentaApplication.java
@@ -0,0 +1,13 @@
+package com.task.penta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PentaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PentaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/penta/PentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.penta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PentaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
