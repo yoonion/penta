3a8289189b9cc71418b25840f0843101c20b1475
yoonion <yoonion97@gmail.com>
2024-10-02 19:59:53 +0900

    feat: 회원 조회 API 구현

--- src/main/java/com/task/penta/service/SystemUserService.java
+++ src/main/java/com/task/penta/service/SystemUserService.java
@@ -1,12 +1,38 @@
 package com.task.penta.service;
 
+import com.task.penta.dto.SystemUserSearchResponseDto;
+import com.task.penta.entity.SystemUser;
 import com.task.penta.repository.SystemUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SystemUserService {
 
     private final SystemUserRepository systemUserRepository;
+
+    public List<SystemUserSearchResponseDto> getUsers(String userId, String userNm) {
+        List<SystemUser> users;
+
+        // 조건에 따라 사용자 조회 (파라미터가 있는 경우 -> 해당 조건으로 검색하고, 파라미터가 없는 경우 모든 회원을 반환)
+        // 회원ID & 회원 이름을 모두 입력하지 않은 경우
+        if (userId != null && !userId.isEmpty() && userNm != null && !userNm.isEmpty()) {
+            users = systemUserRepository.findByUserIdAndUserNm(userId, userNm);
+        } else if (userId != null && !userId.isEmpty()) { // userId만 입력한 경우
+            users = systemUserRepository.findByUserId(userId);
+        } else if (userNm != null && !userNm.isEmpty()) { // userNm만 입력한 경우
+            users = systemUserRepository.findByUserNm(userNm);
+        } else {
+            users = systemUserRepository.findAll(); // 모든 사용자 조회
+        }
+
+        // SystemUser 객체 DTO로 변환
+        return users.stream()
+                .map(SystemUserSearchResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
