2aec57016aed72e368f52b7dc2c70c6946f9d154
yoonion <yoonion97@gmail.com>
2024-10-02 22:27:03 +0900

    docs: controller API별 주석 추가

--- src/main/java/com/task/penta/controller/SystemUserController.java
+++ src/main/java/com/task/penta/controller/SystemUserController.java
@@ -18,6 +18,12 @@ public class SystemUserController {
 
     private final SystemUserService systemUserService;
 
+    /**
+     * 회원 조회 API
+     * @param userId (선택사항) 회원 ID
+     * @param userNm (선택사항) 회원 이름
+     * @return 조회된 회원 정보의 리스트를 JSON 형식으로 응답합니다. List<SystemUserSearchResponseDto> 형식입니다.
+     */
     @GetMapping
     public ResponseEntity<List<SystemUserSearchResponseDto>> getUsers(
             @RequestParam(required = false) String userId,
@@ -29,7 +35,9 @@ public class SystemUserController {
 
     /**
      * 회원 추가 API
-     * @return
+     * @param requestDto 회원 추가에 필요한 정보가 담긴 DTO.
+     *                   JSON 형식으로 userId, userPw, userNm, userAuth를 요청 body에 포함해야 합니다.
+     * @return 추가된 회원의 기본 정보를 JSON 형식으로 응답합니다. SystemUserCreateResponseDto 형식입니다.
      */
     @PostMapping
     public ResponseEntity<SystemUserCreateResponseDto> createUser(@RequestBody SystemUserCreateRequestDto requestDto) {
