4344777f043ed23d8f69937f301db10faf80db4a
yoonion <yoonion97@gmail.com>
2024-10-03 17:24:19 +0900

    feat: 회원 추가 API validation 적용

--- src/main/java/com/task/penta/dto/SystemUserCreateRequestDto.java
+++ src/main/java/com/task/penta/dto/SystemUserCreateRequestDto.java
@@ -1,13 +1,22 @@
 package com.task.penta.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class SystemUserCreateRequestDto {
+
+    @NotBlank
     private final String userId;
+
+    @NotBlank
     private final String userPw;
+
+    @NotBlank
     private final String userNm;
+
+    @NotBlank
     private final String userAuth;
 }
