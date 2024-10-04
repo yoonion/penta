cb4e7d98ad9c4b599ae9f761f816d2c9ee90960b
yoonion <yoonion97@gmail.com>
2024-10-04 17:51:52 +0900

    feat: Spring security 설정 bean 추가

--- src/main/java/com/task/penta/config/WebSecurityConfig.java
+++ src/main/java/com/task/penta/config/WebSecurityConfig.java
@@ -1,15 +1,48 @@
 package com.task.penta.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf((csrf) -> csrf.disable());
+
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+        );
+
+        // 로그인 사용
+        http.formLogin((formLogin) ->
+                formLogin
+                        // 커스텀 로그인 View 설정 (GET /user/login)
+                        .loginPage("/user/login")
+                        // 로그인 처리 (POST /api/user/login)
+                        .loginProcessingUrl("/api/user/login")
+                        // 로그인 처리 후 성공 시 URL
+                        .defaultSuccessUrl("/")
+                        // 로그인 처리 후 실패 시 URL
+                        .failureUrl("/user/login?error")
+                        .permitAll()
+        );
+
+        return http.build();
     }
 }
