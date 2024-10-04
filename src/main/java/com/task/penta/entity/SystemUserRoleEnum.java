994bcddfaf2af865c5a1fde7e43e250846f393c2
yoonion <yoonion97@gmail.com>
2024-10-04 15:43:47 +0900

    feat: JwtUtil 클래스 작성

---
+++ src/main/java/com/task/penta/entity/SystemUserRoleEnum.java
@@ -0,0 +1,20 @@
+package com.task.penta.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum SystemUserRoleEnum {
+    USER(Authority.USER),  // 사용자 권한
+    ADMIN(Authority.ADMIN);  // 관리자 권한
+
+    private final String authority;
+
+    SystemUserRoleEnum(String authority) {
+        this.authority = authority;
+    }
+
+    public static class Authority {
+        public static final String ADMIN = "SYSTEM_ADMIN";
+        public static final String USER = "SYSTEM_USER";
+    }
+}
