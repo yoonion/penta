872f456c76facf875366d33c3361afe7cc9caafb
yoonion <yoonion97@gmail.com>
2024-10-06 21:00:58 +0900

    feat: 공통 헤더 html 작성, 회원 목록 admin 권한만 보이도록 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO `SYSTEM_USER` (`user_idx`, `user_id`, `user_pw`, `user_nm`, `user_auth`) VALUES
-(1, 'admin', '', '관리자', 'SYSTEM_ADMIN');
+(1, 'admin', '$2a$10$2hlN1smKkFs/m6NeXQeVu.HAZR2xCFCsESxz78d3fiy1VeOMIbrcu', '관리자', 'SYSTEM_ADMIN');
