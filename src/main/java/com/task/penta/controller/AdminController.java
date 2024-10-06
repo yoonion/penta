8f6374b5f83f4957445a57bf7506c112e6d1529d
yoonion <yoonion97@gmail.com>
2024-10-06 21:33:46 +0900

    feat: 회원 목록 페이지 구현

--- src/main/java/com/task/penta/controller/AdminController.java
+++ src/main/java/com/task/penta/controller/AdminController.java
@@ -1,15 +1,27 @@
 package com.task.penta.controller;
 
+import com.task.penta.dto.SystemUserSearchResponseDto;
+import com.task.penta.service.SystemUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final SystemUserService systemUserService;
+
     @GetMapping("/users")
-    public String listUsers() {
+    public String listUsers(Model model) {
+        List<SystemUserSearchResponseDto> users = systemUserService.getUsers("", "");
+        model.addAttribute("users", users);
+
         return "admin/user-list";
     }
 }
