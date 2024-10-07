d49e68e812f90257fe3d049a7ca8938f2b2dbda3
yoonion <yoonion97@gmail.com>
2024-10-08 02:18:38 +0900

    refactor: data.sql 일반 회원명 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,8 @@
 INSERT INTO `SYSTEM_USER` (`user_idx`, `user_id`, `user_pw`, `user_nm`, `user_auth`) VALUES
 (1, 'admin', '$2a$10$2hlN1smKkFs/m6NeXQeVu.HAZR2xCFCsESxz78d3fiy1VeOMIbrcu', '관리자', 'SYSTEM_ADMIN');
 INSERT INTO `SYSTEM_USER` (`user_idx`, `user_id`, `user_pw`, `user_nm`, `user_auth`) VALUES
-(2, 'test1', '$2a$10$2hlN1smKkFs/m6NeXQeVu.HAZR2xCFCsESxz78d3fiy1VeOMIbrcu', 'user1', 'SYSTEM_USER');
+(2, 'test1', '$2a$10$2hlN1smKkFs/m6NeXQeVu.HAZR2xCFCsESxz78d3fiy1VeOMIbrcu', '일반회원', 'SYSTEM_USER');
 INSERT INTO `SYSTEM_USER` (`user_idx`, `user_id`, `user_pw`, `user_nm`, `user_auth`) VALUES
-(3, 'test2', '$2a$10$2hlN1smKkFs/m6NeXQeVu.HAZR2xCFCsESxz78d3fiy1VeOMIbrcu', 'user2', 'SYSTEM_USER');
+(3, 'test2', '$2a$10$2hlN1smKkFs/m6NeXQeVu.HAZR2xCFCsESxz78d3fiy1VeOMIbrcu', '일반회원', 'SYSTEM_USER');
 INSERT INTO `SYSTEM_USER` (`user_idx`, `user_id`, `user_pw`, `user_nm`, `user_auth`) VALUES
-(4, 'test3', '$2a$10$2hlN1smKkFs/m6NeXQeVu.HAZR2xCFCsESxz78d3fiy1VeOMIbrcu', 'user3', 'SYSTEM_USER');
+(4, 'test3', '$2a$10$2hlN1smKkFs/m6NeXQeVu.HAZR2xCFCsESxz78d3fiy1VeOMIbrcu', '일반회원3', 'SYSTEM_USER');
