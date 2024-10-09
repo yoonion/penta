2dd87208cb0e6aa46e77c81efc906bcd0af14a14
yoonion <yoonion97@gmail.com>
2024-10-09 18:05:01 +0900

    feat: 회원가입 validation 조건 추가

--- src/main/java/com/task/penta/dto/request/UserUpdateRequestDto.java
+++ src/main/java/com/task/penta/dto/request/UserUpdateRequestDto.java
@@ -1,11 +1,13 @@
 package com.task.penta.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserUpdateRequestDto {
 
     @NotBlank(message = "수정할 회원의 이름을 입력해주세요.")
+    @Size(max = 50, message = "회원 이름은 50자 이하로 입력해주세요.")
     private String userNm;
 }
